c9d0d84d0e7a19348229709e53a5d1ad01f132a1
Rex0626 <kaitotham@gmail.com>
2025-10-11 18:55:16 +0800

    新增不改寫.sh檔案的指令

--- run_incremental.sh
+++ run_incremental.sh
@@ -2,9 +2,9 @@
 echo "--- 執行增量更新爬蟲 ---"
 
 # 執行靜態爬蟲，爬取 8 頁
-python src/scrape_static.py --pages 8
+python dual_source_scraper/src/scrape_static.py --pages 8
 
 # 執行動態爬蟲，爬取 8 頁
-python src/scrape_dynamic.py --pages 8
+python dual_source_scraper/src/scrape_dynamic.py --pages 8
 
 echo "--- 增量更新完成 ---"
