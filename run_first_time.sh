166c466b22b5eab8cd47ec651c03ac07f44de6dc
Rex0626 <kaitotham@gmail.com>
2025-10-15 20:27:42 +0800

    新增新的動態爬蟲

--- run_first_time.sh
+++ run_first_time.sh
@@ -7,4 +7,7 @@ python dual_source_scraper/src/scrape_static.py --pages 5
 # 執行動態爬蟲，爬取 5 頁
 python dual_source_scraper/src/scrape_dynamic.py --pages 5
 
+# 執行新的動態爬蟲，爬取 100 筆
+python dual_source_scraper/src/scrape_new_dynamic.py --limit 100
+
 echo "--- 首次爬取完成 ---"
--- run_incremental.sh
+++ run_incremental.sh
@@ -7,4 +7,7 @@ python dual_source_scraper/src/scrape_static.py --pages 8
 # 執行動態爬蟲，爬取 8 頁
 python dual_source_scraper/src/scrape_dynamic.py --pages 8
 
+# 執行新的動態爬蟲，爬取 100 筆
+python dual_source_scraper/src/scrape_new_dynamic.py --limit 100
+
 echo "--- 增量更新完成 ---"
